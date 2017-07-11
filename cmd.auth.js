c132abb482932a8aed311e0d9b6af467febcaf6d
Geoffrey <geo.galagaran@gmail.com>
2017-07-11 20:29:48 +0800

    Create authAPI function

---
+++ cmd.auth.js
@@ -0,0 +1,29 @@
+const prompt = require("prompt")
+
+const schema = {
+  properties: {
+    consumer_key: {
+      message: 'Enter Twitter API Consumer Key',
+      required: true
+    },
+    consumer_secret: {
+      message: 'Enter Twitter API Consumer Secret',
+      required: true
+    },
+    access_token_key: {
+      message: 'Enter Twitter API Access Token Key',
+      required: true
+    },
+    access_token_secret: {
+      message: 'Enter Twitter API Access Token Secret',
+      required: true
+    }
+  }
+}
+
+const authAPI = (callback) => {
+  prompt.start()
+  prompt.get(schema, (err, result) => callback(result))
+}
+
+export default authAPI
