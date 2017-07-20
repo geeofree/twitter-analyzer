5c8d75b279fa6bed25c5e50d1f3e23031a8e971d
Geoffrey <geo.galagaran@gmail.com>
2017-07-20 14:15:50 +0800

    Create basic server

---
+++ TwitterAPI/server.twitter.js
@@ -0,0 +1,7 @@
+import express from 'express'
+const app = express()
+const PORT = 8080
+
+app.get('/', (req, res) => res.send('Hello world'))
+
+app.listen(PORT, () => console.log(`Listening to LOCALHOST:${PORT}`))
