c41b7e4ea72d83119a1c7ab186c7608a146c69f1
Geoffrey <geo.galagaran@gmail.com>
2017-07-25 14:56:30 +0800

    Integrate server with socket.io

--- TwitterAPI/server.twitter.js
+++ TwitterAPI/server.twitter.js
@@ -1,13 +1,22 @@
 import express from 'express'
-import apiRoutes from './routes/api.routes'
+import http from 'http'
+import apiRoutes from './routes/routes.api'
+import socketIO from 'socket.io'
 
 const app = express()
+const server = http.Server(app)
+const io = socketIO(server)
+const ENDPOINT = '/api/v1.0'
 const PORT = 8080
 
-app.use('/api/v1.0', apiRoutes)
+app.use(ENDPOINT, apiRoutes)
 
 app.use(express.static('dist'))
 
-app.get('/*', (req, res) => res.sendFile('index.html'))
+app.get('/', (req, res) => res.sendFile('index.html'))
 
-app.listen(PORT, () => console.log(`Listening to LOCALHOST:${PORT}`))
+io.on('connection', (socket) => console.log('user connected'))
+
+server.listen(PORT, () => console.log(`Listening to LOCALHOST:${PORT}`))
+
+export default io
