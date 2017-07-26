e0a7ea11b7d15f0c3a18375ea564a0290aeeb1f1
Geoffrey <geo.galagaran@gmail.com>
2017-07-26 20:25:47 +0800

    Create dynamic background changes on fetch response results

--- src/components/search/layout.search.js
+++ src/components/search/layout.search.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import './search.style.sass'
 
 import SearchInput from './input.search'
 
-export default () => (
-  <div id="search">
+const SearchLayout = ({ userData }) => (
+  <div className={
+    !userData || userData.status === 200 ? 'search' :
+    userData && userData.status >= 400 ? 'search error' : 'search'
+  }>
     <div className="container">
       <SearchInput />
     </div>
   </div>
 )
+
+const mapStateToProps = (state) => ({
+  userData: state.search.userData
+})
+
+export default connect(mapStateToProps)(SearchLayout)
