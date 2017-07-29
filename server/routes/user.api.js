89ad6fbb3d145a2dee721985a029df3020d4d5b5
Geoffrey <geo.galagaran@gmail.com>
2017-07-30 01:41:19 +0800

    Log user request

--- server/routes/user.api.js
+++ server/routes/user.api.js
@@ -10,6 +10,8 @@ const API = TwitterInterface(APIConfig)
 router.get('/:twitterHandle', (req, res) => {
   const { twitterHandle } = req.params
 
+  console.log('called')
+
   API.getUserTweets(twitterHandle)
   .then(data => data.forEach(item => io.emit('receive:tweets', { total: data.length, item })))
   .catch(err => io.emit('receive:tweets:error', err))
