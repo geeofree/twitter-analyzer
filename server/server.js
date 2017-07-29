19d577ca166d72cc0743fe225a92b7ab3eac97de
Geoffrey <geo.galagaran@gmail.com>
2017-07-30 02:18:07 +0800

    Remove HTTPS

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 import express from 'express'
 import http from 'http'
-import https from 'https'
 import apiRoutes from './routes/routes.api'
 import socketIO from 'socket.io'
 
 const app = express()
-const server = process.env.NODE_ENV === 'development' ? http.Server(app) : https.Server(app)
+const server = http.Server(app)
 const io = socketIO(server)
 const ENDPOINT = '/api/v1.0'
 const PORT = process.env.PORT || 8080
