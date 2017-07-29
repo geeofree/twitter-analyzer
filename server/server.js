25c2ae68ae5a8f6d41c3ee84767ca0eef565b07e
Geoffrey <geo.galagaran@gmail.com>
2017-07-29 12:53:51 +0800

    Change PORT to point on production port

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express()
 const server = http.Server(app)
 const io = socketIO(server)
 const ENDPOINT = '/api/v1.0'
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
 app.use(ENDPOINT, apiRoutes)
 
