edd913ad0d27b23ba7bdc6a22373d5c6c9ccef9d
Geoffrey <geo.galagaran@gmail.com>
2017-07-30 14:25:22 +0800

    Domain back to client/dev..

--- src/components/search/input.search.js
+++ src/components/search/input.search.js
@@ -23,10 +23,10 @@ class SearchInput extends Component {
     const { twitterHandle } = this.state
     const { toggleFetch, fetching, receiveData, addProgress, resetProgress } = this.props
 
-    const domain = location.origin
+    const domain = 'http://localhost:8080'
     const endpoint = `${domain}/api/v1.0/user/${twitterHandle}`
 
-    const socket = io.connect()
+    const socket = io.connect(domain)
     const data = []
 
 
