ef4ff4e7f418bba7a2ac8e977ec0619a72419154
Geoffrey <geo.galagaran@gmail.com>
2017-07-30 12:15:27 +0800

    Build app for deployment

--- prod/server/routes/user.api.js
+++ prod/server/routes/user.api.js
@@ -29,12 +29,20 @@ router.get('/:twitterHandle', function (req, res) {
   var twitterHandle = req.params.twitterHandle;
 
 
-  API.getUserTweets(twitterHandle).then(function (data) {
-    return data.forEach(function (item) {
-      return _server2.default.emit('receive:tweets', { total: data.length, item: item });
-    });
-  }).catch(function (err) {
-    return _server2.default.emit('receive:tweets:error', err);
+  API.getUserTweets(twitterHandle, function (api_response) {
+    if (api_response.status === 201) {
+      api_response.data.forEach(function (data) {
+        return _server2.default.emit('receive:tweets', {
+          data: data,
+          max_batch: api_response.max_batch,
+          status: api_response.status
+        });
+      });
+    } else if (api_response.status === 200) {
+      _server2.default.emit('receive:tweets', { status: api_response.status });
+    } else if (api_response.status >= 400) {
+      _server2.default.emit('receive:tweets:error', api_response);
+    }
   });
 });
 
--- prod/server/twitter/interface.twitter.js
+++ prod/server/twitter/interface.twitter.js
@@ -15,52 +15,93 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 exports.default = function (APIConfig) {
   var _twitter = new _twitter3.default(APIConfig);
 
-  var getUserTweets = function getUserTweets(twitterHandle) {
-    return new Promise(function (resolve, reject) {
-      var endpoint = 'statuses/user_timeline.json';
-      var params = { screen_name: twitterHandle, count: 200, include_rts: false };
-      var posts = [];
+  var getUserTweets = function getUserTweets(twitterHandle, callback) {
+    var endpoint = 'statuses/user_timeline.json';
+    var params = { screen_name: twitterHandle, count: 200, include_rts: false };
+    var posts = [];
 
-      var getData = function getData(err, tweets) {
-        if (err) {
-          reject({ status: 404, status_message: 'Data not found.' });return;
-        }
+    var getData = function getData(err, tweets) {
+      var last_item = tweets[tweets.length - 1];
 
-        if (posts.length === 0 && tweets.length === 0) {
-          reject({ status: 404, status_message: 'Data not found.' });
-          return;
-        } else if (tweets.length === 0) {
-          resolve(posts);return;
-        }
+      if (err) {
+        callback({ status: 503, data: 'Something went wrong with the request' });
+        return;
+      }
 
-        var max_data = 1000;
-        var lastItem = tweets[tweets.length - 1];
-        var limit = lastItem.user.status_count < max_data ? lastItem.user.status_count : max_data;
+      // Stream is finish when last item is undefined
+      if (last_item === undefined) {
+        callback({ status: 200 });
+        return;
+      }
 
-        if (params.max_id && params.max_id === lastItem.id) {
-          resolve(posts);return;
-        }
-        params.max_id = lastItem.id;
+      // Stop recursion when the last ID is still the same id from the old iteration
+      if (params.max_id && params.max_id === last_item.id) return;
+      params.max_id = last_item.id;
 
-        if (posts.length + tweets.length < limit) {
-          posts.push.apply(posts, _toConsumableArray(tweets));
-        } else {
-          posts.push.apply(posts, _toConsumableArray(tweets.filter(function (_, index) {
-            return index < limit - posts.length;
-          })));
-        }
+      var max_data = 1000;
+      var status_count = last_item.user.statuses_count;
+      var limit = status_count < max_data ? status_count : max_data;
 
-        if (posts.length < limit) {
-          _twitter.get(endpoint, params, getData);
-          return;
-        }
+      // Throw an error when an account exists but has never tweeted
+      if (posts.length === 0 && tweets.length === 0) {
+        callback({ status: 404, data: 'No data found' });
+        return;
+      }
 
-        resolve(posts);
-      };
+      if (posts.length + tweets.length < limit) {
+        posts.push.apply(posts, _toConsumableArray(tweets));
+        callback({ status: 201, data: tweets, max_batch: limit });
+      } else {
+        // Store tweets that is around the index of limit - 1
+        var final_tweets_batch = tweets.filter(function (_, index) {
+          return index < limit - posts.length;
+        });
+        posts.push.apply(posts, _toConsumableArray(final_tweets_batch));
+        // Send final batch of tweets
+        callback({ status: 201, data: final_tweets_batch, max_batch: limit });
+      }
 
-      _twitter.get(endpoint, params, getData);
-    });
+      if (posts.length < limit) {
+        _twitter.get(endpoint, params, getData);
+        return;
+      }
+
+      // Stream is finished
+      callback({ status: 200 });
+    };
+
+    _twitter.get(endpoint, params, getData);
   };
 
   return { getUserTweets: getUserTweets };
 };
+
+// if(err) { callback({}); return; }
+//
+// if(posts.length === 0 && tweets.length === 0) {
+//   reject({ status: 404, status_message: 'Data not found.' })
+//   return
+// }
+// else if(tweets.length === 0) { resolve(posts); return; }
+//
+// const max_data = 1000
+// const lastItem = tweets[tweets.length - 1]
+// const limit = lastItem.user.status_count < max_data ? lastItem.user.status_count : max_data
+//
+// if(params.max_id && params.max_id === lastItem.id) { resolve(posts); return; }
+// params.max_id = lastItem.id
+//
+// if(posts.length + tweets.length < limit) {
+//   posts.push(...tweets)
+// }
+// else {
+//   posts.push(...tweets.filter((_, index) => index < limit - posts.length))
+// }
+//
+//
+// if(posts.length < limit) {
+//   _twitter.get(endpoint, params, getData)
+//   return
+// }
+//
+// resolve(posts)
